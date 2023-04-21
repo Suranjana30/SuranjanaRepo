4b1389265dfef8263f207c11c3de18b28801f5b6
itsmesneha <87040207+itsmesneha@users.noreply.github.com>
2023-04-21 11:11:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello')
+  res.send('Hello Sneha')
 })
 
-app.listen(4000, ()=>{
+app.listen(5000, ()=>{
     console.log("Started server");
 }
 )
